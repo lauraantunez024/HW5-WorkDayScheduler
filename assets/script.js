e6a336724b0688a079ede6ec71794ec6ddf05f23
Laura Antunez <lauraantunez024@gmail.com>
2022-04-07 16:57:59 -0400

    added color coding

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,13 @@ var container = document.querySelector(".container")
 
 var time = moment().format('dddd, MM/DD/YYYY, h:mm');
   $("#currentDay").text(time)
+var currentHour = new Date().getHours();
 
-
-var hours = ['08', '09', '10', '11', '12', '13', '14', '15', '16','17', '18', '19', '20']
+var hours = ['08', '09', '10', '11', '12', '13', '14', '15', '16','17', '18','19']
 
 
 
 hours.forEach(hour=>{
-    console.log(hour)
     var row = document.createElement('div');
     row.classList.add('row', 'time-block');
     row.innerHTML = `
@@ -21,11 +20,44 @@ hours.forEach(hour=>{
     </div>
     <br>
     `
+
+    if(currentHour == hour) {
+        $(".time-block").addClass("present");
+    } else if (hour > currentHour) {
+        $(".time-block").addClass("past");
+    } else if (hour < currentHour) {
+        $(".time-block").addClass("future");
+
+    }
+
     container.appendChild(row);
 
-    
+
 
 
 
 })
+
+// function colorChange() {
+//     var currentHour = new Date().getHours();
+//     for (i=0; i<hours.length; i++) {
+//        if(currentHour == hours[i]) {
+//         $(".time-block").addClass("present");
+//     } else if (currentHour > hours[i]) {
+//         $(".time-block").addClass("future");
+//     } else if (currentHour < hours[i]) {
+//         $(".time-block").addClass("past");
+
+//     }
+
+// }
+// }
+
+
+
+
+
+
+
+
 
